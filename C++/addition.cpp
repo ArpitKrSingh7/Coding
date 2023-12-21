a74bb5dec6d516c9daba2707a7d5ff9a73d37f35
Arpit Kumar Singh <arpitkumarsingh9470@gmail.com>
2023-12-21 21:26:10 +0530

    Create addition.cpp

---
+++ C++/addition.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int x,y;
+    cout<<"Enter first number : ";
+    cin>>x;
+    cout<<"Enter second number : ";
+    cin>>y;
+
+    cout<<"Sum of the numebers is : "<<x+y;
+
+}
