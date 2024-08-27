2e43e3df4da5a844e9474565cbea53235fb7f99c
Arpit Kumar Singh <arpitkumarsingh9470@gmail.com>
2024-08-28 01:20:01 +0530

    Create input.c

---
+++ cs23i1022/input.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "rational.h"
+	
+	
+	rational number1;
+	rational number2;
+	
+	printf("Enter the Numerator of first R.N: ");
+	scanf("%d" , &number1.numerator);
+	
+	printf("Enter the Denominator of first R.N : ");
+	scanf("%d" , &number1.denominator);
+	
+	
+	printf("Enter the Numerator of second R.N: ");
+	scanf("%d" , &number2.numerator);
+	
+	printf("Enter the Denominator of second R.N : ");
+	scanf("%d" , &number2.denominator);
+	
+	
